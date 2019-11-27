863feeeb9bc6bc9de0f3f454a286262b41560970
Kolya Venturi <kolya.venturi@enx3s.com>
2019-11-27 15:16:59 -0700

    Setup additional child types

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Element} from '../constants/types'; 
+import Block from './block';
+import Attachment from './attachment';
 
 type Props = {
-  channel?: string;
   children: string | Element | Element[];
+  channel?: string;
+  replyTo?: string;
+  markdown?: boolean;
+  blocks?: Block[];
+  attachments?: Attachment[];
 };
 
-export default class Message extends React.Component<Props> {
-  transform() {
-    return 'hi';
-  }
-}
+export default class Message extends React.Component<Props> {}
