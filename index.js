b4b864cbd3649f37f13c47984cf6779203fe266b
Kolya Venturi <kolya.venturi@enx3s.com>
2019-12-08 14:03:54 -0700

    Publish steps (#5)
    * Add prepublish step and rename package
    
    * Add README
    
    * Update package name
    
    * Export components separately
    
    * Fix issue with nested import

---
+++ index.js
@@ -0,0 +1,7 @@
+var __importdefault = (this && this.__importdefault) || function (mod) {
+    return (mod && mod.__esmodule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+
+var renderer = __importdefault(require('./lib')).default;
+exports.default = renderer.default;
